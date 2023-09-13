63c885c6c97ed5b3ce49efbc5d6575e23790823f
Dmytro-Lys <d.lysenko122@gmail.com>
2023-09-14 00:19:32 +0300

    fix MobMenu flex

--- src/components/Header/NavList.js
+++ src/components/Header/NavList.js
@@ -3,7 +3,7 @@ const navItems = ["about", "services", "career", "gallery", "contacts"]
 const textHeader = "text-[14px] tracking-[1.4px]"
 const textMenu ="text-[18px] tracking-[1.8px]"
 const NavList = ({flexProp}) => (
-    <ul className={`flex ${flexProp} gap-[24px] xl:gap-[56px]`}>
+    <ul className={`flex ${flexProp} gap-[48px] md:gap-[24px] xl:gap-[56px]`}>
         {navItems.map((item, index) => { return <NavItem key={index} dataItem={item} textProps={flexProp ? textMenu : textHeader} /> })}
     </ul>
 )
